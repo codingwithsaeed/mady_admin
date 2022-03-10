e3ea11a3528d7463d3e4c83675fbec47b50c77b4
Saeed Ahmadi <codingwithsaeed@gmail.com>
2022-03-10 19:59:45 +0330

    woking on offers page

---
+++ lib/features/offer/presentation/pages/add_offer_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AddOfferPage extends StatelessWidget {
+  static const id = 'AddOfferPage';
+  const AddOfferPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
